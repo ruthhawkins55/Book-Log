56aea786ed0b719a0fbd86f0143215e8865d11a0
ruthhawkins55 <ruthiehawkins555@gmail.com>
2025-02-20 21:19:14 -0500

    Added changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  mode: 'production',
-  entry: './script.js',  // Point to your script.js file as the entry
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js',  // The output bundled JavaScript file
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: 'html-loader',  // Process HTML files
-      },
+    mode: 'production',
+    entry: './script.js',  // Point to your script.js file as the entry
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js',  // The output bundled JavaScript file
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: 'html-loader',  // Process HTML files
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',  // Use index.html as the template
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',  // Use index.html as the template
-    }),
-  ],
-};
+    externals: {
+      'firebase/app': 'firebase',
+      'firebase/firestore': 'firebase.firestore',
+      'face-api.js': 'faceapi',  // If you're using face-api.js as a global object
+    },
+  };
+  
 
 
